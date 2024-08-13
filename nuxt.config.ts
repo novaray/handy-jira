54857f4e4185006dfb9cae222ac654451ae23a54
skkim <novaray@naver.com>
2024-08-13 18:24:40 +0900

    ui library 설치 및 nuxt 4 설정 적용.
    - quasar library 설치.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
   compatibilityDate: '2024-04-03',
+  modules: ['nuxt-quasar-ui'],
   devtools: { enabled: true }
-})
+});
