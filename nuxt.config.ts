e5b964c193bc2997a51f8034603276afaea6eea4
skkim <novaray@naver.com>
2024-08-14 11:42:34 +0900

    nuxt i18n route warning 제거.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ export default defineNuxtConfig({
   modules: ['nuxt-quasar-ui', '@nuxtjs/i18n'],
   i18n: {
     vueI18n: './locales/i18n.config.ts',
-    locales: ['en', 'ko']
+    locales: ['en', 'ko'],
+    strategy: 'prefix' // 해당 설정이 없으면 No match found for location 경고가 계속 발생. https://github.com/nuxt-modules/i18n/pull/2894/files
   },
   devtools: { enabled: true }
 });
