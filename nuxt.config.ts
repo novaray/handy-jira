54b78c4e5588edb769872734058c5d3aeaecbe75
skkim <novaray@naver.com>
2024-08-16 12:26:02 +0900

    scss 설치 및 페이지 레이아웃 스타일 설정, quasar notify 설정.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,20 @@ export default defineNuxtConfig({
     compatibilityVersion: 4
   },
   compatibilityDate: '2024-04-03',
-  modules: ['nuxt-quasar-ui', '@nuxtjs/i18n', "@pinia/nuxt"],
+  modules: ['nuxt-quasar-ui', '@nuxtjs/i18n', '@pinia/nuxt'],
+  quasar: {
+    plugins: ['Notify'],
+    config: {
+      notify: {
+        position: 'top'
+      }
+    }
+  },
   i18n: {
     vueI18n: './locales/i18n.config.ts',
     locales: ['en', 'ko'],
     strategy: 'prefix' // 해당 설정이 없으면 No match found for location 경고가 계속 발생. https://github.com/nuxt-modules/i18n/pull/2894/files
   },
+  css: ['@/assets/scss/main.scss'],
   devtools: { enabled: true }
 });
