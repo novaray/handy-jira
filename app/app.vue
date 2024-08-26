f94c6492fa317db5f5996165f9c3af2879f3c65e
skkim <novaray@naver.com>
2024-08-26 15:52:53 +0900

    jira items가 있을 시에 새로고침, 브라우저 종료 방지.

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+const testExecutionStore = useTestExecutionStore();
+const onPreventUnload = (event: BeforeUnloadEvent) => {
+  if (!testExecutionStore.jiraItems.length) {
+    return;
+  }
+
+  event.returnValue = '';
+  event.preventDefault();
+};
+
+onMounted(() => {
+  window.addEventListener('beforeunload', onPreventUnload);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('beforeunload', onPreventUnload);
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
